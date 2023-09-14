048f8bdf5256452b6064099db1660c4f5cf282a4
Nivetha <nivetha@datasense.in>
2023-09-14 18:38:29 +0530

    ios build in 14/09/2023

--- lib/screens/viewallreviewimages.dart
+++ lib/screens/viewallreviewimages.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_indicator/carousel_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 import 'package:siamdealz/ResponseModule/StoreDetailsModule/StoreDetailsModel.dart';
